4562b302def22f7c72f108bab75d11aa5ce28787
Paul <appcellon@gmail.com>
2020-04-20 02:43:14 +0300

    added logo

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 import Head from 'next/head'
+import fetch from 'isomorphic-unfetch'
 
-export default function Home() {
+const Home = ({ pages }) => {
+  console.log(pages)
+
   return (
     <div className="container">
-      <h1>This is my home page</h1>
+      {pages.map(page => (
+        <div>
+        {page.title}
+        </div>
+      ))}
     </div>
   )
 }
+
+
+
+export async function getServerSideProps() {
+  const { API_URL } = process.env
+
+  const res = await fetch(`${API_URL}/pages`)
+  const data = await res.json()
+
+  return {
+      props: {
+          pages: data
+      }
+  }
+}
+
+export default Home
