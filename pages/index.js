84374c6d1f8973fac3b9d52bf21bd5a20db20212
Paul <appcellon@gmail.com>
2020-04-20 04:23:33 +0300

    changed to a new API hosted on heroku

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,14 @@
 import Head from 'next/head'
 import fetch from 'isomorphic-unfetch'
 
-const Home = ({ pages }) => {
-  console.log(pages)
+const Home = ({ posts }) => {
+  console.log(posts)
 
   return (
     <div className="container">
-      {pages.map(page => (
+      {posts.map(post => (
         <div>
-        {page.title}
+        {post.title}
         </div>
       ))}
     </div>
@@ -20,12 +20,12 @@ const Home = ({ pages }) => {
 export async function getServerSideProps() {
   const { API_URL } = process.env
 
-  const res = await fetch(`${API_URL}/pages`)
+  const res = await fetch(`${API_URL}/posts`)
   const data = await res.json()
 
   return {
       props: {
-          pages: data
+          posts: data
       }
   }
 }
